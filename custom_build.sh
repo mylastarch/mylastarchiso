500c096cfceb4a628de517912c3599fd87f3249c
mylastarch <mylastarch@dccathome.com>
2026-03-18 20:48:32 -0400

    update of config files

---
+++ custom_build.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+airootfs=(airootfs/etc)
+
+#Grub
+mkdir -p "$airootfs/default"
+cp -r "/etc/default/grub" "$airootfs/default"
+
+# wheel Group
+mkdir -p "$airootfs/sudoers.d"
+g_wheel=($airootfs/sudoers.d/g_wheel)
+echo "%wheel ALL=(ALL:ALL) ALL" > $g_wheel
+
+#Symbolic Links
+##Network Manager
+mkdir -p "$airootfs/systemd/system/multi-user.target.wants"
+ln -sv "/usr/lib/systemd/system/NetworkManager.service" "$airootfs/systemd/system/multi-user.target.wants"
+
+mkdir -p "$airootfs/systemd/system/network-online.target.wants"
+ln -sv "/usr/lib/systemd/system/NetworkManager-wait-online.service" "$airootfs/systemd/system/network-online.target.wants"
+
+ln -sv "/usr/lib/systemd/system/NetworkManager-dispatcher.service" "$airootfs/systemd/dbus.org.freedesktop.dispatcher.service"
+
+## Bluetooth
+ln -sv "/usr/lib/systemd/system/bluetooth.service" "$airootfs/systemd/system/network-online.target.wants"
+
+## Graphical target
+ln -sv "/usr/lib/systemd/system/graphical.target" "$airootfs/systemd/system/default.target"
+
+## SDDM
+ln -sv "/usr/lib/systemd/system/sddm.service" "$airootfs/systemd/system/display-manager.service"
+
+# SDDM conf
+mkdir -p "#airootfs/sddm.conf.d"
+sed -n '1,35p' /usr/lib/sddm/sddm.conf.d/default.conf > $airootfs/sddm.conf
+sed -n '38,137p' /usr/lib/sddm/sddm.conf.d/default.conf > $airootfs/sddm.conf.d/kde_settings.conf
+
+#Desktop Environment
+sed -i 's/Session=/Session=plasma.desktop/' $airootfs/sddm.conf
+
+# Display Server
+sed -i 's/DisplayServer=x11/DisplayServer=wayland/' $airootfs/sddm.conf
+
+# Numlock
+sed -i 's/Numlock=none/Numlock=on/' $airootfs/sddm.conf
+
+# User
+user=default
+sed -i 's/User=/User='$user'/' $airootfs/sddm.conf
+
+## Hostname
+echo mylastarch > $airootfs/hostname 
+
+# Adding the new user
+if grep -q "$user" $airootfs/passwd 2> /dev/null; then
+	echo -e "\nUser Found....."
+else
+	sed -i '1 a\'"$user:x:1000:1000::/home/$user:/usr/bin/bash" $airootfs/passwd
+	echo -e "\nUser not found......"
+fi
+
+# Password
+hash_pd=$(openssl passwd -6 default)
+
+if grep -o "$user" $airootfs/shadow > /dev/null; then
+	echo -e "\nPassword exits, Not Modifying."
+else
+	sed -i '1 a\'"$user:$hash_pd:14871::::::" $airootfs/shadow
+	echo -e "\nModifying the password"
+fi
+
+# Group
+touch $airootfs/group
+echo -e "root:x:0:root\nadm:x:4:$user\nwheel:x:10:$user\nuucp:x:14:$user\n$user:x:1000:$user" > $airootfs/group 
+
+# gshadow
+touch $airootfs/gshadow
+echo -e "root:!*::root\n$user:!*::" > $airootfs/gshadow
+
+# Grub cfg
+grubcfg=(grub/grub.cfg)
+sed -i 's/default=archlinux/default=mylastarch/' $grubcfg
+sed -i 's/timeout=15/timeout=10/' $grubcfg
+sed -i 's/menuentry "Arch/menuentry "mylastarch/' $grubcfg
+
+if ! grep -q 'archisosearchuuid=%ARCHISO_UUID% cow_spacesize=10G copytoram=n' $grubcfg 2> /dev/null; then
+	sed -i 's/archisosearchuuid=%ARCHISO_UUID%/archisosearchuuid=%ARCHISO_UUID% cow_spacesize=10G copytoram=n/' $grubcfg
+fi
+
+if ! grep -q '#play' $grubcfg 2> /dev/null; then
+	sed -i 's/play/#play/' $grubcfg
+fi
+
+# entries
+efiloader=(efiboot/loader)
+sed -i 's/Arch/mylastarch/' $efiloader/entries/01-archiso-linux.conf
+sed -i 's/Arch/mylastarch/' $efiloader/entries/02-archiso-speech-linux.conf
+#loader 
+sed -i 's/timeout 15/timeout 10/' $efiloader/loader.conf
+sed -i 's/beep on/beep off/' $efiloader/loader.conf
+
+
+
+
+
+
+
